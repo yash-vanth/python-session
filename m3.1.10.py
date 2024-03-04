35a9ccb87a89e08c028497762ac73b47aaad43da
= <=>
2024-03-04 18:40:49 +0530

    module 3.1.10 commit

---
+++ m3.1.10.py
@@ -0,0 +1,7 @@
+n=str(input())
+if n=="spathiphyllum":
+   print("No, I want a big Spathiphyllum!")
+elif n=="Spathiphyllum":
+    print("Yes - Spathiphyllum is the best plant ever!") 
+else:
+    print("Spathiphyllum! Not", n+"!")
