8030129553cf71914a8fd3a4d6e699274d99c0d4
= <=>
2024-03-04 17:28:01 +0530

    module 3.2.17 commit

---
+++ three.py
@@ -0,0 +1,5 @@
+for ch in "john.smith@pythoninstitute.org":
+    if ch == "@":
+        break
+    print(ch,end="")
+ 
---
+++ two.py
@@ -0,0 +1,6 @@
+x=1
+while x<=10:
+    if(x%2!=0):
+        print(x)
+    x+=1
+    
